7c88e6ff756af1b48123cf303e99655ea0a7cbc7
Dillan444 <rojasdylan444@gmail.com>
2023-08-19 12:55:13 -0500

    List added

--- src/app/component/heroes/heroes.component.ts
+++ src/app/component/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/models/hero.model';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,5 @@ import { Hero } from 'src/app/models/hero.model';
 export class HeroesComponent {
   name = ''
 
-  hero: Hero = {
-    id: 1,
-    name: 'Super Man',
-    gender: 'Hombre',
-    height: 1.90,
-    weight: 94,
-    powers: ['Fuerza', 'Volar', 'Velocidad', 'Visión calorica', 'Visión de rayos X'],
-    weakness: 'Criptonita',
-    weapons: null,
-    image: 'https://www.cinemascomics.com/wp-content/uploads/2023/08/Superman-Legacy.jpg'
-  }
+  heroes: Hero[] = HEROES
 }
